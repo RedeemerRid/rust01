d839a2e7bd0d279947239a7e2dc83844da25c428
RedeemerRid <akhmatovich@gmail.com>
2024-08-02 16:42:45 +0300

    first commit

---
+++ rust001/target/debug/deps/rust001.d
@@ -0,0 +1,5 @@
+D:\RUST\1\rust01\rust001\target\debug\deps\rust001.exe: src/main.rs
+
+D:\RUST\1\rust01\rust001\target\debug\deps\rust001.d: src/main.rs
+
+src/main.rs:
---
+++ rust001/target/debug/rust001.d
@@ -0,0 +1,1 @@
+D:\RUST\1\rust01\rust001\target\debug\rust001.exe: D:\RUST\1\rust01\rust001\src\main.rs
